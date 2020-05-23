4275592edd7512142751240a7f23b4c9a0d491c1
Hayden Mirza <haydenjwmirza@gmail.com>
2020-05-23 19:16:34 -0400

    Handlers for guildMemberAdd and guildMemberRemove have been added

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const config = require('./config.json')
+//const config = require('./config.json')
 
 const Discord = require('discord.js')
 const client = new Discord.Client()
@@ -7,10 +7,30 @@ client.on('ready', () => {
     console.log("--- NAEK Bot Activated ---")
 })
 
+//Handles when a member joins
+//The channel id is hardcoded to the Hideout server bot test channel.
+client.on('guildMemberAdd', (member) => {
+    console.log("New User Joined");
+    client.channels.fetch('713873076374208512').then((channel) => {
+        channel.send("Welcome " + member.user.username + "!");
+    }).catch(console.error);
+});
+
+//Handles when a member leaves
+//The channel id is hardcoded to the Hideout server bot test channel.
+client.on('guildMemberRemove', (member) => {
+    console.log("New User Joined");
+    client.channels.fetch('713873076374208512').then((channel) => {
+        channel.send("Goodbye " + member.user.username + "!");
+    }).catch(console.error);
+});
+
+
 client.on('message', (msg) => {
     if(msg.content.charAt(0) != '-') return
     let msgSplit = msg.content.split()
     msg.channel.send('Command received.')
 })
 
-client.login(config.discordKey)
+//client.login(config.discordKey)
+client.login('NzEzODE1MzcwNzMzMTkxMjYw.Xslmqw.ga7-8rO2iCMdSUmCyIjVuyLGN8Q');
