362bfdf959fff360c63b1d5a6f77ff25113469f4
NAEK2K <naek2k@outlook.com>
2020-05-23 19:08:02 -0400

    added persistent database functionality (lowdb)

--- bot.js
+++ bot.js
@@ -1,5 +1,15 @@
 const config = require('./config.json')
 
+// lowdb
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+// lowdb init
+db.defaults({guilds: []}).write()
+
+// discord.js
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
@@ -7,8 +17,12 @@ client.on('ready', () => {
     console.log("--- NAEK Bot Activated ---")
 })
 
+client.on('guildCreate', (guild) => {
+    db.get('guilds').push({id: guild.id, rankSettings: {joinable: {}}, welcomeSettings: {channel: '', message: ''}}).write()
+})
+
 client.on('message', (msg) => {
-    if(msg.content.charAt(0) != '-') return
+    if(msg.content.charAt(0) != '-') return // ignore messages that aren't commands
     let msgSplit = msg.content.split()
     msg.channel.send('Command received.')
 })
