19f76224c902a01c395827a6a4c06d37d79b1f2e
NAEK2K <naek2k@outlook.com>
2020-05-24 10:07:35 -0400

    added more settings to welcome/goodbye, added dev

--- bot.js
+++ bot.js
@@ -22,30 +22,53 @@ client.on('message', (msg) => {
     let msgSplit = msg.content.split(' ')
     console.log(msgSplit)
     if(msgSplit[0] == '-website') {
-        let testMessage = `test to ping you <@${msg.author.id}>`
+        let testMessage = "Hey <#713942346760323152>"
         msg.channel.send("Developer's Site: https://naek.ca")
         msg.channel.send(testMessage)
     }
-    if(msgSplit[0] == '-set-welcome-channel') {
+    // welcome settings
+    if(msgSplit[0] == '-set-welcome-channel') { // set channel
         db.get('guilds').find({id: msg.guild.id}).assign({welcomeChannel: msgSplit[1]}).write()
         msg.channel.send("Welcome channel updated!")
     }
-    if(msgSplit[0] == '-set-welcome-message') {
+    if(msgSplit[0] == '-set-welcome-message') { // set message
         db.get('guilds').find({id: msg.guild.id}).assign({welcomeMessage: msg.content.substr(21)}).write()
         msg.channel.send("Welcome message updated!")
     }
-    if(msgSplit[0] == '-set-goodbye-channel') {
+    if(msgSplit[0] == '-set-welcome') { // set status
+        if(msgSplit[1] == 'enable' || msgSplit[1] == 'disable') {
+            db.get('guilds').find({id: msg.guild.id}).assign({welcomeEnable: (msgSplit[1] == 'enable') ? 1 : 0}).write()
+            msg.channel.send(`Welcome has been ${(msgSplit[1] == 'enable') ? 'enabled' : 'disabled'}.`)
+        } else {
+            msg.channel.send("Invalid parameter. -set-welcome (enable|disable)")
+        }
+    }
+    // goodbye settings
+    if(msgSplit[0] == '-set-goodbye-channel') { // set channel
         db.get('guilds').find({id: msg.guild.id}).assign({goodbyeChannel: msgSplit[1]}).write()
         msg.channel.send("Goodbye channel updated!")
     }
-    if(msgSplit[0] == '-set-goodbye-message') {
+    if(msgSplit[0] == '-set-goodbye-message') { // set message
         db.get('guilds').find({id: msg.guild.id}).assign({goodbyeMessage: msg.content.substr(21)}).write()
         msg.channel.send("Goodbye message updated!")
     }
+    if(msgSplit[0] == '-set-goodbye') { // set status
+        if(msgSplit[1] == 'enable' || msgSplit[1] == 'disable') {
+            db.get('guilds').find({id: msg.guild.id}).assign({goodbyeEnable: (msgSplit[1] == 'enable') ? 1 : 0}).write()
+            msg.channel.send(`Goodbye has been ${(msgSplit[1] == 'enable') ? 'enabled' : 'disabled'}.`)
+        } else {
+            msg.channel.send("Invalid parameter. -set-goodbye (enable|disable)")
+        }
+    }
+    // development functions only
+    if(msgSplit[0] == '-db-refresh') {
+        db.get('guilds').remove({id: msg.guild.id}).write()
+        db.get('guilds').push({id: msg.guild.id, welcomeChannel: '', welcomeMessage: '', welcomeEnable: 1, goodbyeChannel: '', goodbyeMessage: '', goodbyeEnable: 1, joinableRanks: [], adminRanks: []}).write()
+        msg.channel.send("Database has been refreshed.")
+    }
 })
 
-//Handles when a member joins
-//The channel id is hardcoded to the Hideout server bot test channel.
+// on member join
 client.on('guildMemberAdd', (member) => {
     let welcomeChannel = db.get('guilds').find({id: member.guild.id}).get('welcomeChannel').value()
     let welcomeMessage = db.get('guilds').find({id: member.guild.id}).get('welcomeMessage').value()
@@ -54,8 +77,7 @@ client.on('guildMemberAdd', (member) => {
     }).catch(console.error);
 });
 
-//Handles when a member leaves
-//The channel id is hardcoded to the Hideout server bot test channel.
+// on member leave
 client.on('guildMemberRemove', (member) => {
     let goodbyeChannel = db.get('guilds').find({id: member.guild.id}).get('goodbyeChannel').value()
     let goodbyeMessage = db.get('guilds').find({id: member.guild.id}).get('goodbyeMessage').value()
@@ -64,6 +86,7 @@ client.on('guildMemberRemove', (member) => {
     }).catch(console.error);
 });
 
+// on guild join
 client.on('guildCreate', (guild) => {
     db.get('guilds').push({id: guild.id, welcomeChannel: '', welcomeMessage: '', welcomeEnable: 1, goodbyeChannel: '', goodbyeMessage: '', goodbyeEnable: 1, joinableRanks: [], adminRanks: []}).write()
 })
